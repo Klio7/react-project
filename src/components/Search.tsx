bf3967f22bbad42606487e2defcfb2cc0e98bb9d
Anna Odintsova <Klio7@mail.ru>
2024-07-05 18:31:56 +0300

    feat: add search input

---
+++ src/components/Search.tsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export class Search extends Component {
+  render() {
+    return (
+      <div>
+        <input></input>
+        <button>SEARCH</button>
+      </div>
+    );
+  }
+}
